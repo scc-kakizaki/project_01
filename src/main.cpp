e71764728a3920cb73238ad7a35220b43572b296
scc-kakizaki <shun.kakizaki@scc-kk.co.jp>
2025-02-27 14:07:25 +0900

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "gtest/gtest.h"
 
-int main(int argc, char **argv)
-{
-	testing::InitGoogleTest(&argc, argv);	// GoogleTestの初期化
-	return RUN_ALL_TESTS();		// 全てのテストを実行
+int main(int argc, char **argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
